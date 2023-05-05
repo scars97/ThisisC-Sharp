52513560ea96f709767bd03024b94c3723380a5e
scars97 <qkr970504@gmail.com>
2023-05-05 14:19:18 +0900

    Thread, Async

---
+++ 19장/Async/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    internal class Program
+    {
+        async static private void MyMethodAsync(int count)
+        {
+            Console.WriteLine("C");
+            Console.WriteLine("D");
+
+            await Task.Run(async () =>
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    Console.WriteLine($"{i}/{count} ...");
+                    await Task.Delay(100);
+                }
+            });
+
+            Console.WriteLine("G");
+            Console.WriteLine("H");
+        }
+
+        static void Caller()
+        {
+            Console.WriteLine("A");
+            Console.WriteLine("B");
+
+            MyMethodAsync(3);
+
+            Console.WriteLine("E");
+            Console.WriteLine("F");
+        }
+
+        static void Main(string[] args)
+        {
+            Caller();
+
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ 19장/BasicThread/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BasicThread
+{
+    internal class Program
+    {
+        static void DoSomething()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"DoSomething : {i}");
+                // Sleep()메서드를 만나면 인수만큼 CPU 사용 멈춤.
+                Thread.Sleep(10);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(new ThreadStart(DoSomething));
+
+            Console.WriteLine("Starting thread...");
+            t1.Start();
+
+            // 위의 메서드가 실행되는 동시에 메인 스레드 반복문도 실행.
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Main : {i}");
+                Thread.Sleep(10);
+            }
+
+            Console.WriteLine("Wating until thread stops...");
+            t1.Join();
+
+            Console.WriteLine("Finished");
+        }
+    }
+}
+
